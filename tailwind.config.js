21b6214bf045d7aed4cffc7ae7e88fa917e8e1a0
chrjl <73675826+chrjl@users.noreply.github.com>
2023-07-23 23:38:56 -0700

    style: google font import - Inter, Arimo
    Import google fonts in global CSS. Set global CSS root font family to
    Inter. Set Tailwind CSS default sans-serif to Arimo.
    
    The page should use Inter everywhere that `sans-serif` is not specified.
    The intention is to use Inter for the UI (for the right arrow ligature),
    and manually set `sans-serif` in content containers.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    fontFamily: {
+      sans: ['Arimo', 'Arial', 'Helvetica', 'sans-serif', 'ui-sans-serif'],
+      monospace: ['Cousine', 'monospace', 'ui-monospace'],
+    },
     extend: {},
   },
   plugins: [],
