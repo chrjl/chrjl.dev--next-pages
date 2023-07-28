6bbb22dfd7e58365cd3be49bb01a1314099e6aff
chrjl <73675826+chrjl@users.noreply.github.com>
2023-07-27 22:06:42 -0700

    style: add page title
    Use next/head element to add title to index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,45 +2,51 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
+import Head from 'next/head';
 import Card from '../components/Card';
 
 export default function Page({ portfolio }) {
   return (
-    <div className="container max-w-screen-sm mx-auto my-8 whitespace-pre-line font-sans">
-      <header className="my-8 text-center">
-        <h1 className="mb-4 text-4xl font-bold">Christopher Lee</h1>
-        <h2 className="text-xl font-semibold">
-          JavaScript Developer
-          <div className="text-base font-medium">
-            AWS Certified Solutions Architect Associate
-          </div>
-        </h2>
-        <h3 className="text-sm mt-2">
-          Los Angeles, California
-          <span className="px-4">|</span>
-          <a
-            className="text-blue-600 underline"
-            href="https://github.com/chrjl"
-            target="_blank"
-          >
-            <img
-              src="/github-mark.svg"
-              alt=""
-              className="inline-block align-middle h-4 mr-1"
-            />
-            chrjl
-          </a>
-        </h3>
-      </header>
+    <>
+      <Head>
+        <title>Christopher Lee | Portfolio</title>
+      </Head>
+      <div className="container max-w-screen-sm mx-auto my-8 whitespace-pre-line font-sans">
+        <header className="my-8 text-center">
+          <h1 className="mb-4 text-4xl font-bold">Christopher Lee</h1>
+          <h2 className="text-xl font-semibold">
+            JavaScript Developer
+            <div className="text-base font-medium">
+              AWS Certified Solutions Architect Associate
+            </div>
+          </h2>
+          <h3 className="text-sm mt-2">
+            Los Angeles, California
+            <span className="px-4">|</span>
+            <a
+              className="text-blue-600 underline"
+              href="https://github.com/chrjl"
+              target="_blank"
+            >
+              <img
+                src="/github-mark.svg"
+                alt=""
+                className="inline-block align-middle h-4 mr-1"
+              />
+              chrjl
+            </a>
+          </h3>
+        </header>
 
-      <ul>
-        {portfolio.map((project) => (
-          <li key={project.data.title} className="m-8">
-            <Card {...project} />
-          </li>
-        ))}
-      </ul>
-    </div>
+        <ul>
+          {portfolio.map((project) => (
+            <li key={project.data.title} className="m-8">
+              <Card {...project} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
